09a3894209e22a0b72562a39f74ae52318fc138b
kapila <kapilacst@gmail.com>
2018-10-09 15:40:09 +0530

    These are my3D Unity Game files

---
+++ game/Assets/scripts/playerCollosion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class playerCollosion : MonoBehaviour {
+
+	public playermovement movement;
+
+	void OnCollisionEnter (Collision collisionInfo){
+
+		if (collisionInfo.collider.tag == "obstacle") {
+		
+			movement.enabled = false;
+		}
+	}
+}
